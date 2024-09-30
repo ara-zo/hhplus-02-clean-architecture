4108aaa20ae7a02636e7911cac0f2d496dfc6d8a
ara-zo <jo91kr@gmail.com>
2024-09-30 22:43:36 +0900

    :tada: Initial commit

---
+++ src/main/kotlin/io/hhplus/clean_architecture/Hhplus02CleanArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package io.hhplus.clean_architecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Hhplus02CleanArchitectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<Hhplus02CleanArchitectureApplication>(*args)
+}
